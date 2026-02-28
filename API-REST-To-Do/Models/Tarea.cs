372f8c80f6b31bea1f167b846915abdebaea8068
Anthony HL <reseanthonyy@gmail.com>
2026-02-28 13:42:20 -0600

    Creación del modelo tarea, iinstlación de paquetes NuGet

---
+++ API-REST-To-Do/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace API_REST_To_Do.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public bool isCompleted { get; set; }
+
+    }
+}
